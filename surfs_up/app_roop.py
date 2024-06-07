93299636650dda96b7bf95fce134078a87813eb4
rooprg <richardroop.pmp@icloud.com>
2024-06-07 18:45:42 -0400

    Late Day 3 Updates

--- surfs_up/app_roop.py
+++ surfs_up/app_roop.py
@@ -1,30 +1,75 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine, func, inspect
+from sqlalchemy.orm import Session
 
+from flask import Flask, jsonify
+
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-
+# Create an engine
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all the available routes:"""
+    return (
+        f"Available Routes-<br/>"
+        f"            <br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v.1.0/start/end"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    rain = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date > last_year).\
+        order_by(Measurement.date).all()
+
+    # Create a list of dicts with `date` and `prcp` as the keys and values
+    rain_totals = []
+    for result in rain:
+        row = {}
+        row["date"] = result[0]
+        row["prcp"] = result[1]
+        rain_totals.append(row)
+
+            return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
