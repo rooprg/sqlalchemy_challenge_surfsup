1e12c4807f6a798aaf91e3301938b7e44759329d
rooprg <richardroop.pmp@icloud.com>
2024-06-08 16:24:01 -0400

    Early Day 4 Updates

--- surfs_up/app_roop.py
+++ surfs_up/app_roop.py
@@ -42,34 +42,56 @@ def welcome():
     """List all the available routes:"""
     return (
         f"Available Routes-<br/>"
-        f"            <br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/start<br/>"
-        f"/api/v.1.0/start/end"
+        f"/api/v1.0/start/end"
     )
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   
+   one_year_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   
+   precip_subset = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= one_year_date).all()
+   
+   precip_table = {date: prcp for date, prcp in precip_subset}
+   
+   session.close()
+   
+   return jsonify(precip_table)
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    session = Session(engine)
+
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tempobs():
     
-    rain = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date > last_year).\
-        order_by(Measurement.date).all()
+    session = Session(engine)
+    
+    results = 
+    
 
-    # Create a list of dicts with `date` and `prcp` as the keys and values
-    rain_totals = []
-    for result in rain:
-        row = {}
-        row["date"] = result[0]
-        row["prcp"] = result[1]
-        rain_totals.append(row)
 
-            return jsonify(all_passengers)
+
+
+
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
